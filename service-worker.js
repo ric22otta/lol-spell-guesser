80b9253835d9a0a810fb31b39c26fc92432f3395
ric22otta <riccardo.buonocore08@gmail.com>
2026-07-14 11:30:58 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const APP_CACHE = 'lol-spell-guesser-app-v4.0.0';
+const APP_CACHE = 'lol-spell-guesser-app-v4.0.2';
 const RUNTIME_CACHE = 'lol-spell-guesser-runtime-v4';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './css/style.css',
+  './css/style.css?v=4.0.2',
   './js/constants.js',
   './js/storage.js',
   './js/data.js',
   './js/ui.js',
   './js/game.js',
-  './js/app.js',
+  './js/app.js?v=4.0.2',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-maskable-512.png',
